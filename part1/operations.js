376e135490d482d586b0ff58168dc7f3fb18120e
Senpai <clevercoder0307@gmail.com>
2025-12-01 00:56:02 +0530

    assignment and comparison operators in javascript

---
+++ part1/operations.js
@@ -0,0 +1,21 @@
+let plusition = 5 + 5
+let subtract = 6 - 3
+let division = 8 / 2
+let multiplication = 2 * 8
+let remainder = 16 % 5
+let exponential = 2 ** 4
+
+let plus = 100
+plus++
+
+let minus = 100
+minus--
+
+let num1 = 2
+let num2 = 4
+let num3 = 6
+
+const answer1 = num1 == num2  // equal to
+const answer2 = num1 != num2  // not equal to
+const answer3 = num1 >= num2  // greater than equal to
+const answer4 = num1 <= num2  // lesser than equal to
