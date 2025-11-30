78ce5ad3207ecf080b91a883306da7c46315fde2
Senpai <clevercoder0307@gmail.com>
2025-12-01 00:22:01 +0530

    changes in let and const

---
+++ part1/changes.js
@@ -0,0 +1,10 @@
+// let can be assigned a new value later
+// const are fixed at the time of assigning the value and cant be changed or update later on
+
+let userName = "Senpai"
+userName = "CleverCoder"
+// This is valid
+
+const nameValue = "Senpai"
+nameValue = "CleverCoder"
+// This will throw an error: TypeError: Assignment to constant variable
