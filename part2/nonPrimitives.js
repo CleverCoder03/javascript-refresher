7a304545ba7562c7111bc403dddf7610f4da4c70
Senpai <clevercoder0307@gmail.com>
2025-12-01 23:22:59 +0530

    non primitive types

---
+++ part2/nonPrimitives.js
@@ -0,0 +1,18 @@
+const userData = {
+    "first name": "Senpai",
+    age: 22,
+    isGenius: false
+}
+
+console.log(userData["first name"]);
+console.log(userData.isGenius);
+
+// These are the 2 ways of calling object dats
+// Also i can make changes to data and even add some more data in object even though the object is stored using const variable
+
+
+// ARRAY
+
+let arr = ["senpai", 22, true]
+console.log(arr[1]);
+
