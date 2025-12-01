5f930c063fb629b92f01f7dec4ce3d110b513e41
Senpai <clevercoder0307@gmail.com>
2025-12-01 21:57:20 +0530

    added primitives

---
+++ part1/primitives.js
@@ -0,0 +1,13 @@
+let name = "vishal"
+let userName = new String("vishal")
+
+// Both are correct but the first one has type of string and second one is object.
+
+let number = 22
+let userNumber = new Number(22)
+
+let boolean = true
+let userBoolean = new Boolean(true)
+
+let sym = Symbol()
+let userSym = new Symbol() // This is not possible causse there isn't any Symbol() constructor
