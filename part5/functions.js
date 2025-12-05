46e474f6e977a1df868eba3c83488d58e3a9bff4
Senpai <clevercoder0307@gmail.com>
2025-12-05 12:27:46 +0530

    solved 5 function problems

---
+++ part5/functions.js
@@ -0,0 +1,87 @@
+/*
+1. Write a function named `makeTea' that takes one
+parameter, `type0fTea , and returns a string like
+`"Making green tea" when called with '"green tea"'.
+Store the result in a variable named `teaOrder'.
+*/
+
+// function makeTea(typeOfTea) {
+//     return `Making ${typeOfTea}`
+// }
+
+// let teaOrder = makeTea("Herbal tea")
+// console.log(teaOrder);
+
+
+/*
+2. Create a function named `orderTea' that takes one
+parameter, `teaType'. Inside this function, create
+another function named `confirmOrder' that returns a
+message like '"Order confirmed for chai".
+Call 'confirmOrder' from within `orderTea' and return the
+result.
+*/
+
+// function orderTea(teaType) {
+//     function confirmOrder() {
+//         return `Order confirmed for ${teaType}`
+//     }
+//     return confirmOrder()
+// }
+
+// console.log(orderTea("chai"))
+
+
+/*
+3. Write an arrow function named `calculateTotal that
+takes two parameters: `price' and `quantity . The
+function should return the total cost by multiplying the
+`price' and `quantity .
+Store the result in a variable named `totalCost'.
+*/
+
+// const calculateTotal = (price, quantity) => {
+//     return `The total cost is ${price * quantity}`
+// }
+
+// const totalCost = calculateTotal(500,5)
+// console.log(totalCost)
+
+
+/*
+4. Write a function named `processTeaOrder' that takes
+another function, `makeTea', as a parameter and calls it
+with the argument `"earl grey"'.
+Return the result of calling `makeTea'.
+*/
+
+// function makeTea(typeOfTea) {
+//     return `makeTea: ${typeOfTea}`
+// }
+
+// function processTeaOrder(fn) {
+//     return fn("earl grey")
+// }
+
+// console.log(processTeaOrder(makeTea));
+
+
+
+/*
+5. Write a function named `createTeaMaker' that returns
+another function. The returned function should take one
+parameter, `teaType', and return a message like '"Making
+green tea".
+Store the returned function in a variable named
+`teaMaker' and call it with '"green tea".
+*/
+
+// function createTeaMaker () {
+//     return function (teaType) {
+//         return `Making ${teaType}`
+//     }
+// }
+
+// let teaMaker = createTeaMaker()
+
+// console.log(teaMaker("chai"))
