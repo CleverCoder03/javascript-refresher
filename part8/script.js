d2595b5649325074ca907f006680914e09f56bfc
Senpai <clevercoder0307@gmail.com>
2025-12-07 01:22:32 +0530

    solved remaining 3 problemsof DOM

--- part8/script.js
+++ part8/script.js
@@ -1,8 +1,33 @@
-document.getElementById('changeTextButton').addEventListener('click', function () {
-    document.getElementById('myParagraph').textContent = "This is new paragraph"
-})
+document
+  .getElementById("changeTextButton")
+  .addEventListener("click", function () {
+    document.getElementById("myParagraph").textContent =
+      "This is new paragraph";
+  });
 
+document
+  .getElementById("highlightFirstCity")
+  .addEventListener("click", function () {
+    document
+      .getElementById("citiesList")
+      .firstElementChild.classList.add("highlight");
+  });
 
-document.getElementById('highlightFirstCity').addEventListener('click', function () {
-    document.getElementById('citiesList').firstElementChild.classList.add('highlight')
-})
+document.getElementById("changeOrder").addEventListener("click", function () {
+  let coffee = document.getElementById("coffeeType");
+  coffee.textContent = "Espresso";
+  coffee.style.backgroundColor = "red";
+});
+
+document.getElementById("addNewItem").addEventListener("click", function () {
+  let newItem = document.createElement("li");
+  newItem.textContent = "Eggs";
+
+  document.getElementById("shoppingList").appendChild(newItem);
+});
+
+document
+  .getElementById("removeLastTask")
+  .addEventListener("click", function () {
+    document.getElementById("taskList").lastElementChild.remove();
+  });
