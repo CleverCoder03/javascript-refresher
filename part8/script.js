e52307d256ee24ea6b7dda6ca5e126420aa184ba
Senpai <clevercoder0307@gmail.com>
2025-12-07 20:56:37 +0530

    solved remaining 5 DOM problems

--- part8/script.js
+++ part8/script.js
@@ -31,3 +31,44 @@ document
   .addEventListener("click", function () {
     document.getElementById("taskList").lastElementChild.remove();
   });
+
+document.getElementById("clickMeButton").addEventListener("click", function () {
+  alert("Are you Senpai?!");
+});
+
+document.getElementById("teaList").addEventListener("click", function (e) {
+  console.log(e.target.textContent.toUpperCase());
+  if (e.target && e.target.matches(".teaItem")) {
+    alert(`Selected item is ${e.target.textContent.toUpperCase()}`);
+  }
+});
+
+document
+  .getElementById("feedbackForm")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let label =
+      document.getElementById("feedbackForm").firstElementChild.textContent;
+
+    let newP = document.createElement("p");
+    newP.textContent = label.toUpperCase();
+
+    newP.style.color = "#fff";
+
+    document.getElementById("example-8").appendChild(newP);
+
+    let value = document.getElementById("feedbackInput").value.toUpperCase();
+
+    document.getElementById("feedbackDisplay").textContent = value;
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("domStatus").textContent = "DOM fully loaded";
+});
+
+document
+  .getElementById("toggleHighlight")
+  .addEventListener("click", function () {
+    document.getElementById("descriptionText").classList.toggle("highlight");
+  });
