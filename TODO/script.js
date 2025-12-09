c5e1e474d089a49ff8918b6dc6817ffd305bc4a7
Senpai <clevercoder0307@gmail.com>
2025-12-09 21:28:00 +0530

    created first task and stored successfully

--- TODO/script.js
+++ TODO/script.js
@@ -1,3 +1,19 @@
-document.getElementById('todo-input')
-document.getElementById('add-task-btn')
-document.getElementById('todo-list')
+const todoInput = document.getElementById('todo-input')
+const addTaskButton = document.getElementById('add-task-btn')
+const todoList = document.getElementById('todo-list')
+
+let tasks = []
+
+addTaskButton.addEventListener('click', () => {
+    const inputData = todoInput.value.trim()
+    if(inputData === "") return
+
+    const newTask = {
+        id: Date.now(),
+        task: inputData,
+        completed: false
+    }
+
+    tasks.push(newTask)
+    console.log(tasks)
+})
