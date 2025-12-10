bf25af22b826848f0b22802227f73e68b6322e4d
Senpai <clevercoder0307@gmail.com>
2025-12-10 13:21:20 +0530

    Developed TODO List

--- TODO/script.js
+++ TODO/script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const todoInput = document.getElementById("todo-input");
   const addTaskButton = document.getElementById("add-task-btn");
-  const todoList = document.getElementById("todo-list");
+  const todoList = document.getElementById("task-list");
 
-  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-  tasks.forEach(task => renderTasks(task))
+  tasks.forEach((task) => renderTasks(task));
 
   addTaskButton.addEventListener("click", () => {
     const inputData = todoInput.value.trim();
@@ -21,10 +21,37 @@ document.addEventListener("DOMContentLoaded", () => {
     saveTasks();
     todoInput.value = "";
     console.log(tasks);
-  });
+    renderTasks(newTask)
+});
 
   function renderTasks(task) {
-    console.log(task);
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed) li.classList.add('completed')
+    li.innerHTML = `
+        <span>${task.task}</span>
+        <button>Delete</button>
+    `;
+    li.addEventListener('click', (e) => {
+        if(e.target.tagName === "BUTTON") return
+
+        task.completed = !task.completed
+        li.classList.toggle('completed')
+        saveTasks()
+
+    })
+
+    li.querySelector("button").addEventListener('click', (e) => {
+        e.stopPropagation()
+
+        tasks = tasks.filter(t => t.id !== task.id)
+        li.remove()
+        saveTasks()
+    })
+    todoList.appendChild(li);
+
+    
+
   }
 
   function saveTasks() {
